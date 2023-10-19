3fe35a93a95cd6b2b0c0175babe53458db21a9b9
StrategistDJ <148493449+StrategistDJ@users.noreply.github.com>
2023-10-19 23:12:09 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N_ROWS 50
+#define MAX_L 20
+#define FILENAME "../meetings.csv"
+
+//definizione di struct per il salvataggio dei dati
+typedef struct{
+    char giorno[MAX_L], mese[MAX_L], anno[MAX_L];
+}calendario;
+
+typedef struct {
+    char riunione[MAX_L], tipo[MAX_L];
+    char nome[MAX_L], posto[MAX_L];
+    calendario data;
+}associato;
+
+associato *readFile(FILE* fin, int *tot);
+int *ricercaAss(associato *vettore,char *nome_ass, int tot);
+associato *ordinaRes(associato *vettore, int numero);
+void stampaRes(associato *vettore, int *res, int numero);
+
+int main() {
+    FILE* fin;
+    char input[10];
+    associato *v;
+    int *res_v, i, cnt_ass, cnt_tot;
+    int flag = 1; //flag utilizzato per evitare l'indice zero contenuto nel vettore del risultato
+//apertura del file
+    fin = fopen (FILENAME, "r");
+
+    if( fin == NULL){
+        printf("Errore apertura del file!!");
+        return -1;
+    }
+//conta il numero totale di righe presenti nel file
+    cnt_tot = 0;
+    v = readFile(fin, &cnt_tot);
+
+    /*v = ordinaRes(v, cnt_tot);*/
+
+    /*for(int j=0; j<cnt_tot; j++){
+        printf(" %s-%s-%s\n", v[j].data.anno, v[j].data.mese, v[j].data.giorno);
+    }*/
+
+    printf("Area da considerare: ");
+    scanf(" %s", input);
+
+    res_v = ricercaAss(v, input, cnt_tot);
+
+//conta il numero di associazioni che sono state trovate
+    cnt_ass = 0;
+    for(i=0; res_v[i]!=0 || flag;i++){
+        if(res_v[i] == 0){
+            flag = 0;
+        }
+        cnt_ass++;
+    }
+    stampaRes(v, res_v, cnt_ass);
+
+    free(res_v);
+    free(v);
+    return 0;
+}
+
+//legge il file e lo salva in un vettore di struct create precedentemente
+associato *readFile(FILE *fin, int *tot) {
+    associato *m;
+    int i=0;
+    char parola[MAX_L];
+
+    m = malloc(N_ROWS * sizeof(associato));
+
+    while(!feof(fin)) {
+        fscanf(fin, "%s %[^;];%[^/]/%[^/]/%[^;];%s", m[i].riunione, parola, m[i].data.giorno, m[i].data.mese, m[i].data.anno, m[i].posto);
+        sscanf(parola, " %s %[^\n]", m[i].tipo, m[i].nome);
+        i++;
+    }
+    *tot = i;
+
+    return m;
+}
+
+//ricerca nel vettore di struct l'associazione inserita da tastiera
+//mettere in un vettore risultato che contiene gli indici delle associazioni cercate
+int *ricercaAss(associato *vettore, char *nome_ass, int tot){
+    int i, j=0, *res;
+
+    res = calloc (tot, sizeof(int));
+
+    for(i=0; i<tot; i++){
+        if(strstr(vettore[i].nome, nome_ass) != NULL || strcmp(vettore[i].tipo, "Generale") == 0){
+            res[j] = i;
+            j++;
+        }
+    }
+    return res;
+}
+/*
+//riordina la struct di vettori letti dal file
+ (Per motivi tempistici non ho potuto finire la funzione di ordinamento, mi dispiace)
+associato *ordinaRes(associato *vettore, int numero){
+
+}*/
+//infine questa funziona stampa il risultato su console
+void stampaRes(associato *vettore, int *res, int numero){
+    int i;
+
+    for(i=0; i<numero; i++){
+        if(strlen(vettore[res[i]].nome)>2){
+            printf("RIUNIONE %s %s\t\t %s-%s-%s \t%s\n", vettore[res[i]].tipo,  vettore[res[i]].nome,  vettore[res[i]].data.anno, vettore[res[i]].data.mese,
+                   vettore[res[i]].data.giorno,  vettore[res[i]].posto);
+        }
+        else{
+            printf("RIUNIONE %s %s\t\t\t\t %s-%s-%s \t%s\n", vettore[res[i]].tipo,  vettore[res[i]].nome,  vettore[res[i]].data.anno, vettore[res[i]].data.mese,
+                   vettore[res[i]].data.giorno,  vettore[res[i]].posto);
+        }
+    }
+}
